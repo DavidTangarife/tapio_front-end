b50a3a3006827c3d501de761fea8612adc0883cb
JacobsDevs <jacobdevsmail@gmail.com>
2025-05-30 10:46:04 +1000

    add: Access-Control-Allow-Origin on get emails

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -7,10 +7,17 @@ import TestEmails from "./routes/TestEmails";
 // import Filter from "./routes/Filter"
 
 const router = createBrowserRouter([
-  { loader: async () => {
-    const response = await fetch("http://localhost:3000/getemails");
-    console.log(response.status);
-      return response.json(); }, path: "/emails", Component: TestEmails },
+  {
+    loader: async () => {
+      const response = await fetch("http://localhost:3000/getemails", {
+        headers: {
+          'Access-Control-Allow-Origin': '127.0.0.1'
+        }
+      });
+      console.log(response.status);
+      return response.json();
+    }, path: "/emails", Component: TestEmails
+  },
   { path: "/", Component: Landing },
   { path: "/setup", Component: SetupForm },
   { path: "/home", Component: Home },
