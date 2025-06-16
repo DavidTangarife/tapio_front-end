aaebe08d772751787e09f738f38a38cf6976ff41
DSTV <9871@holbertonstudents.com>
2025-06-16 18:36:57 +1000

    Project Dropdown

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,5 +1,4 @@
 import { createBrowserRouter } from "react-router";
-
 import Landing from "./routes/Landing";
 import SetupForm from "./routes/AccountSetUp";
 import Home from "./routes/Home";
@@ -9,33 +8,49 @@ import ViewEmail from "./routes/ViewEmail";
 // import Filter from "./routes/Filter"
 
 const router = createBrowserRouter([
-  {
-    loader: async ({ params }) => {
-      const response = await fetch(`http://localhost:3000/api/projects/${params.projectId}/emails`, {
-        credentials: "include",
-      });
-      const data = await response.json();  // parse JSON body (the emails array)
-      return data;
-    },
-    path: "/projects/:projectId/emails",
-    Component: EmailSection,
-  },
   { path: "/", Component: Landing },
   { path: "/setup", Component: SetupForm },
-  { path: "/projects/:projectId/home", Component: Home },
-  { path: "/:projectId/email/:emailId", Component: ViewEmail },
+
   {
-    loader: async ({ params }) => {
-      const projectId = params.projectId;
-      if (!projectId) {
-        throw new Error("Missing project ID in route params");
-      }
-      return { _id: projectId };
-    },
-    path: "/kanban/:projectId",
-    Component: Kanban,
+    path: "/projects/:projectId",
+    children: [
+      { path: "home", Component: Home },
+      {
+        loader: async ({ params }) => {
+          const projectId = params.projectId;
+          if (!projectId) {
+            throw new Error("Missing project ID in route params");
+          }
+          return { _id: projectId };
+        },
+        path: "kanban",
+        Component: Kanban,
+      },
+      {
+        loader: async ({ params }) => {
+          const response = await fetch(
+            `http://localhost:3000/api/projects/${params.projectId}/emails`,
+            {
+              credentials: "include",
+            }
+          );
+          const data = await response.json(); // parse JSON body (the emails array)
+          return data;
+        },
+        path: "inbox",
+        Component: EmailSection,
+      },
+      // { path: "screener", Component: filter }
+      { path: "inbox/:emailId", Component: ViewEmail }, // its supposed to be /:projectId/email/:emailId
+    ],
   },
-  // { path: '/filter', Component: Filter}
+  { path: "/:projectId/email/:emailId", Component: ViewEmail },
+  // If the user is already in the databbase he doesn't need to go through create a prject but directly to home
+  // fix the routes so that all have the project id before the inbox, kanban, and screener
+  // make sure each project has its own inbox, kanban, screnner
+  // work on the css for the buttoms inbox, kanban,  screener to look better places
+  // star working on the pop up of the opportunity and retrieve the color and icon
+  // there is a quick small change in the user menu goes toU really quickly when swapping from email/kanban and screener
 ]);
 
 export default router;
--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,13 +1,16 @@
 import Button from "./Button";
 import "./Header.css";
+import { Project } from "../../types/types";
 import { useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [projectOpen, setProjectOpen] = useState(false);
   const [fullName, setFullName] = useState("");
+  const [projects, setProjects] = useState<Project[]>([]);
   const navigate = useNavigate();
-  const {projectId} = useParams();
+  const { projectId } = useParams();
 
   useEffect(() => {
     const fetchUser = async () => {
@@ -15,27 +18,42 @@ const Header = () => {
         const res = await fetch("http://localhost:3000/api/full-name", {
           credentials: "include",
         });
-        const data = await res.json();
-        if (res.ok && data.fullName) {
-          setFullName(data.fullName);
+        const userData = await res.json();
+        if (res.ok && userData.fullName) {
+          setFullName(userData.fullName);
         }
       } catch (err) {
-        console.error("Failed to fetch user", err)
+        console.error("Failed to fetch user", err);
+      }
+    };
+
+    const fetchProjects = async () => {
+      try {
+        const res = await fetch("http://localhost:3000/api/user-projects", {
+          credentials: "include",
+        });
+        const projectData = await res.json();
+        if (res.ok) {
+          setProjects(projectData.projects);
+        }
+      } catch (err) {
+        console.error("Failed to fetch projects", err);
       }
     };
     fetchUser();
+    fetchProjects();
   }, []);
 
   const getInitials = (name: string) => {
     const names = name.trim().split(" ");
     if (names.length === 1) return names[0][0].toUpperCase();
     return `${names[0][0]}${names[names.length - 1][0]}`.toUpperCase();
-  }
+  };
 
   const userInitials = getInitials(fullName || "U");
 
   const toggleMenu = () => {
-    setMenuOpen(prev => !prev);
+    setMenuOpen((prev) => !prev);
   };
 
   const handleSettings = () => {
@@ -50,35 +68,85 @@ const Header = () => {
     setMenuOpen(false);
   };
 
-return (
-  <>
-    <section className="header-container">
-      <h1 className="logo">Tapio</h1>
-      <div className="tgl-btn-container">
-        <Button 
-          className={`tgl-btn inbox-tgl-btn ${location.pathname === `/projects/${projectId}/emails` ? "active" : ""}`}
-          onClick={() => navigate(`/projects/${projectId}/emails`)}
-          buttonText="Inbox"
-        />
-        <Button
-          className={`tgl-btn board-tgl-btn ${location.pathname === `/kanban/${projectId}` ? "active" : ""}`}
-          onClick={() => navigate(`/kanban/${projectId}`)}
-          buttonText="Board"
-        />
-      </div>
-      <div className="user-menu">
-        <button className="user-btn" onClick={toggleMenu}>
-          {userInitials}
-        </button>
-        {menuOpen && (
-          <div className="dropdown">
-            <button onClick={handleSettings}>Settings</button>
-            <button onClick={handleLogout}>Logout</button>
+  const currentProject = projects.find((p) => p._id === projectId);
+  const toggleProject = () => {
+    setProjectOpen((prev) => !prev);
+  };
+
+  const createProject = () => {
+    navigate("/setup", {
+      state: { mode: "createProject" },
+    });
+    setProjectOpen(false);
+  };
+
+  const swapProject = (projecIdtomove: string) => {
+    navigate(`/projects/${projecIdtomove}/home`);
+    setProjectOpen(false);
+  };
+
+  return (
+    <>
+      <section className="header-container">
+        <div className="header">
+          <h1 className="logo">Tapio</h1>
+          <div className="project">
+            <button onClick={toggleProject}>{currentProject?.name}</button>
+            {projectOpen && (
+              <div className="dropdown">
+                {projects.map((pro) => {
+                  return (
+                    <button key={pro._id} onClick={() => swapProject(pro._id)}>
+                      {pro.name}
+                    </button>
+                  );
+                })}
+                <button onClick={createProject}>New Project +</button>
+              </div>
+            )}
           </div>
-        )}
-      </div>
-    </section>
-  </>
-)
-}
+          <div className="user-menu">
+            <button className="user-btn" onClick={toggleMenu}>
+              {userInitials}
+            </button>
+            {menuOpen && (
+              <div className="dropdown">
+                <button onClick={handleSettings}>Settings</button>
+                <button onClick={handleLogout}>Logout</button>
+              </div>
+            )}
+          </div>
+        </div>
+
+        <div className="tgl-btn-container">
+          <Button
+            className={`tgl-btn inbox-tgl-btn ${
+              location.pathname === `/projects/${projectId}/inbox`
+                ? "active"
+                : ""
+            }`}
+            onClick={() => navigate(`/projects/${projectId}/inbox`)}
+            buttonText="Inbox"
+          />
+          <Button
+            className={`tgl-btn board-tgl-btn ${
+              location.pathname === `/projects/${projectId}/kanban`
+                ? "active"
+                : ""
+            }`}
+            onClick={() => navigate(`/projects/${projectId}/kanban`)}
+            buttonText="Board"
+          />
+          <Button
+            className={`tgl-btn board-tgl-btn ${
+              location.pathname === `/kanban/${projectId}` ? "active" : ""
+            }`}
+            onClick={() => navigate(`/projects/${projectId}/screener`)}
+            buttonText="Screener"
+          />
+        </div>
+      </section>
+    </>
+  );
+};
 export default Header;
